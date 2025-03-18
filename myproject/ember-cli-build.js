66443f95e173c2026715a3ff7808b73bb36b90e7
Tracy Huang <tmacmm@gmail.com>
2025-03-18 16:47:05 -0500

    init/Initialize the project

---
+++ myproject/ember-cli-build.js
@@ -0,0 +1,26 @@
+'use strict';;
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+const {
+  compatBuild
+} = require("@embroider/compat");
+
+module.exports = async function(defaults) {
+  const {
+    buildOnce
+  } = await import("@embroider/vite");
+
+  const app = new EmberApp(defaults, {
+    'ember-cli-babel': { enableTypeScriptTransform: true },
+
+    // Add options here
+  });
+  return compatBuild(app, buildOnce, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    staticEmberSource: true
+  });
+};
